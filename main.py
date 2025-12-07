ab58a4057dcf5ccd193b0dbcad4d84fda35f6314
galbb12 <galbb12@gmail.com>
2025-12-07 18:57:11 +0200

    Added hello world tool

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from openai import OpenAI
 
+conv = []
+
+def hello_world():
+    print("hello nigga")
+    return "called and printed to the user"
+
+
+def handle_tools():
+    tools = conv[-1].tool_calls
+    if tools:
+        for tool in tools:
+            match tool.function.name:
+                case "hello_world":
+                    conv.append({"role": "tool", "tool_call_id": tool.id, "content": hello_world()})
+
+    
 
 def main():
 
@@ -8,18 +24,45 @@ def main():
         base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
     )
 
-    response = client.chat.completions.create(
-        model="gemini-2.5-flash",
-        messages=[
-            {"role": "system", "content": "Respond only in rhymes."},
-            {
-                "role": "user",
-                "content": input("Enter prompt: ")
-            }
-        ]
-    )
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "hello_world",
+                "description": "Call this function when the user asks to say hello world.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {},  # No parameters needed for this simple tool
+                    "required": [],
+                },
+            },
+        }
+    ]
 
-    print(response.choices[0])
+    while True:
+
+        conv.append([
+                {"role": "system", "content": "You are a helpful assistant."},
+                {
+                    "role": "user",
+                    "content": input("Enter prompt: ")
+                }
+            ])
+
+        response = client.chat.completions.create(
+            model="gemini-2.5-flash",
+            messages = conv,
+            tools=tools,
+            tool_choice="auto"
+        )
+
+        conv.append(response.choices[0].message)
+        handle_tools()
+
+        content = response.choices[0].message.content
+        if(content):
+            print(f"Model response: {content}")
+
 
 if __name__ == "__main__":
     main()
